fdf3fa7373c76729c3116a7b81b7f36a042545d9
Ben Liu <benliu2345@gmail.com>
2024-03-26 11:06:41 -0700

    added framework of js for claimed and posted htmlalso deleted basket page and basket js as we will be incorporating them into claimed.html

---
+++ scripts/posted.js
@@ -0,0 +1,29 @@
+function displayCardsDynamically(collection) {
+    let cardTemplate = document.getElementById("posts"); 
+
+    db.collection(collection).get()
+        .then(allclothes => {
+            //var i = 1;  //Optional: if you want to have a unique ID for each item
+            allclothes.forEach(doc => { //iterate thru each doc
+                var details = doc.data().details;
+                var dropoff = doc.data().dropoff;
+                var itemname = doc.data().item_name;
+                var location = doc.data().location;
+                var meetup = doc.data().meetup;
+                var pickup = doc.data().pickup;
+                let newcard = cardTemplate.content.cloneNode(true);
+                //newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = itemname;
+                //newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-location').innerHTML = location;
+                newcard.querySelector('.card-image').src = doc.data().image;;
+
+
+                //attach to gallery, Example: "items-go-here"
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                //i++;   //Optional: iterate variable to serve as unique ID
+            })
+        })
+}

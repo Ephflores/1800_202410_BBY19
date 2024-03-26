c7ac4709b2958082a99177eb56a432360095110c
Ben Liu <benliu2345@gmail.com>
2024-03-26 10:40:46 -0700

    made results page, and results js

--- scripts/basket.js
+++ scripts/basket.js
@@ -4,25 +4,23 @@
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("mybasket"); // Retrieve the HTML element with the ID "mybasket" and store it in the cardTemplate variable. 
 
-    db.collection(collection).get()   //the collection called "hikes"
-        .then(allHikes => {
+    db.collection(collection).get() 
+        .then(clothes => {
             //var i = 1;  //Optional: if you want to have a unique ID for each item
-            allHikes.forEach(doc => { //iterate thru each doc
-                var title = doc.data().name;       // get value of the "name" key
-                var details = doc.data().details;  // get value of the "details" key
-                var itemCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
-                var docID = doc.id;
-                let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+            clothes.forEach(doc => { //iterate thru each doc
+                var details = doc.data().details;  
+                var dropoff = doc.data().dropoff;    
+                var itemname = doc.data().item_name;
+                var location = doc.data().location;
+                var meetup = doc.data().meetup;
+                var pickup = doc.data().pickup;
+                let newcard = cardTemplate.content.cloneNode(true);
                 newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-text').innerHTML = details;
-                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; 
 
-                //Optional: give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
                 //attach to gallery, Example: "items-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);

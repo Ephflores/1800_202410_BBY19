c204039444aff999538951bf326406171f087d8e
Ben Liu <benliu2345@gmail.com>
2024-04-01 18:51:48 -0700

    added code to history.js

--- scripts/history.js
+++ scripts/history.js
@@ -1,31 +1,48 @@
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("posts");
-    //firebase.auth().onAuthStateChanged(user => 
 
     db.collection(collection).get()
         .then(allclothes => {
             //var i = 1;  //Optional: if you want to have a unique ID for each item
+            const searchParams = new URLSearchParams(window.location.search);
             allclothes.forEach(doc => { //iterate thru each doc
-                var details = doc.data().details;
-                var dropoff = doc.data().dropoff;
-                var itemname = doc.data().item_name;
-                var location = doc.data().location;
-                var meetup = doc.data().meetup;
-                var pickup = doc.data().pickup;
-                let newcard = cardTemplate.content.cloneNode(true);
-                //newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
-                //update title and text and image
-                newcard.querySelector('.card-title').innerHTML = itemname;
-                //newcard.querySelector('.card-text').innerHTML = details;
-                newcard.querySelector('.card-location').innerHTML = location;
-                newcard.querySelector('.card-image').src = doc.data().image;;
-                document.getElementById("originalDivId").setAttribute("id", "newDivId");
+                if (!searchParams.has("categories") &&
+                    (!('claimed' in doc.data()) || !(doc.data().claimed))) {
+                    var itemname = doc.data().item_name;
+                    var location = doc.data().location;
+                    let newcard = cardTemplate.content.cloneNode(true);
+                    //newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+                    //update title and text and image
+                    newcard.querySelector('.card-title').innerHTML = itemname;
+                    //newcard.querySelector('.card-text').innerHTML = details;
+                    newcard.querySelector('.card-location').innerHTML = location;
+                    newcard.querySelector('.card-image').src = doc.data().image;;
+                    newcard.querySelector('.btn').setAttribute("id", doc.id);
+                    //attach to gallery, Example: "items-go-here"
+                    document.getElementById(collection + "-go-here").appendChild(newcard);
+
+                    //i++;   //Optional: iterate variable to serve as unique ID
+
+                }
+                if ((!('claimed' in doc.data()) || !(doc.data().claimed)) //need to fix this
+                    && doc.data().categories == searchParams.get("categories")) {
+                    var itemname = doc.data().item_name;
+                    var location = doc.data().location;
+                    let newcard = cardTemplate.content.cloneNode(true);
+                    //newcard.querySelector('a').href = "eachHike.html?docID=" + docID;
+                    //update title and text and image
+                    newcard.querySelector('.card-title').innerHTML = itemname;
+                    //newcard.querySelector('.card-text').innerHTML = details;
+                    newcard.querySelector('.card-location').innerHTML = location;
+                    newcard.querySelector('.card-image').src = doc.data().image;;
+                    newcard.querySelector('.btn').setAttribute("id", doc.id);
 
 
-                //attach to gallery, Example: "items-go-here"
-                document.getElementById(collection + "-go-here").appendChild(newcard);
+                    //attach to gallery, Example: "items-go-here"
+                    document.getElementById(collection + "-go-here").appendChild(newcard);
 
-                //i++;   //Optional: iterate variable to serve as unique ID
+                    //i++;   //Optional: iterate variable to serve as unique ID
+                }
             })
         })
 }
